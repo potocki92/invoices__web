02808ea802e4881ea57e92d63fc60c405bf8fd5d
potocki92 <mateusz.potocki92@gmail.com>
2022-12-03 20:50:54 +0100

    new commit:
    * added js
      * is-hidden.js allows user to open and close A4 preview
    * changed A4 container and tables from this

---
+++ js/is-hidden.js
@@ -0,0 +1,17 @@
+(() => {
+  const ref = {
+    // open preview
+    open: document.querySelector("[data-preview-open]"),
+    // to add / remove class 'is-hidden'
+    toggle: document.querySelector("[data-preview]"),
+  };
+
+  // click and open preview
+  ref.open.addEventListener("click", toggleModal);
+  // clock and close preview
+
+  // function preview
+  function toggleModal() {
+    ref.toggle.classList.toggle("is-hidden");
+  }
+})();
